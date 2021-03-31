58ba1138f8dc4e8a1a0843841abbd52c523282c4
ivorrr987 <79218038+ivorrr987@users.noreply.github.com>
2021-03-31 21:52:22 +0900

    git commit -m

---
+++ index.js
@@ -0,0 +1,11 @@
+const title = document.querySelector("#title");
+
+function handleClick(){
+    title.style.color = 'blue';
+}
+
+title.addEventListener("click", handleClick);
+
+if (2 === 2 ){
+    console.log('hho');
+}
